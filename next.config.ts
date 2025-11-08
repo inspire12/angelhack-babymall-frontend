18f63591abcfbc5a702e4326b7ac5362dbc8a764
Isaac Moon <dev.chanyongmoon@gmail.com>
2025-11-09 03:31:16 +0900

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',     // 정적 export 사용
     distDir: 'out',       // export 결과 폴더
     basePath: '/app',     // Spring에서 /app/* 으로 서빙
     images: { unoptimized: true }, // export에 필요
